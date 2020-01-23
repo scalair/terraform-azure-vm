85650260928d9c0734bd090bb44bfdaf9ff62631
Mathieu Duczmanski <mduczmanski@scalair.fr>
2020-01-23 16:09:27 +0100

    Updating conditionnal test

--- main.tf
+++ main.tf
@@ -145,7 +145,7 @@ resource "azurerm_virtual_machine" "vm_linux_with_data_disk" {
 }
 
 resource "azurerm_virtual_machine" "vm_windows" {
-  count                 = var.is_windows_vm == "true" || (contains(list(var.vm_os_simple, var.vm_os_offer), "WindowsServer") || contains(list(var.vm_os_simple, var.vm_os_offer), "MicrosoftWindowsDesktop")) && (var.data_disk == "false") ? 1 : 0
+  count                 = var.is_windows_vm == "true" && (contains(list(var.vm_os_simple, var.vm_os_offer), "WindowsServer") || contains(list(var.vm_os_simple, var.vm_os_offer), "MicrosoftWindowsDesktop")) && (var.data_disk == "false") ? 1 : 0
   name                  = var.vm_name
   location              = var.location
   resource_group_name   = var.resource_group_name
